97d444b4fddcf54f0b245247ad0d90f9f017ee86
enzeyu <yez_aisec@163.com>
2023-01-01 15:23:02 +0800

    quick_v1

---
+++ quickSort/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"quickSort/quick"
+)
+
+func main(){
+	fmt.Println(quick.Sort([]int{21,22,21,22,21,22,21,30}))
+}
---
+++ quickSort/quick/sort.go
@@ -0,0 +1,8 @@
+package quick
+
+import "fmt"
+
+func Sort(data []int) int{
+	fmt.Println(data)
+	return 0
+}
